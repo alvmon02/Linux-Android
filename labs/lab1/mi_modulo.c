1002176f918ac7a754b99e28ba0ccb3e84ed6326
alvmon02 <alvmon02@ucm.es>
2024-09-17 12:06:18 +0200

    nuovo file:             conclusions.helpnote ,of first Lab, NOT COMPLETE

--- labs/lab1/mi_modulo.c
+++ labs/lab1/mi_modulo.c
@@ -2,13 +2,16 @@
 #include <linux/kernel.h>	/* Definición de KERN_INFO */
 MODULE_LICENSE("GPL"); 	/*  Licencia del modulo */
 
+int modulo_lin_init(void);
+void modulo_lin_clean(void);
+
 /* Función que se invoca cuando se carga el módulo en el kernel */
 int modulo_lin_init(void)
 {
 	printk(KERN_INFO "Modulo LIN cargado. Hola kernel.\n");
 
 	/* Devolver 0 para indicar una carga correcta del módulo */
-	return 0;
+	return -EINVAL;
 }
 
 /* Función que se invoca cuando se descarga el módulo del kernel */
