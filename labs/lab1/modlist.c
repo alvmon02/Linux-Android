76410c469726a235b0b57b4f9b4e576b16e8a555
alvmon02 <alvmon02@ucm.es>
2024-09-20 11:22:53 +0200

    Estructuras iniciales Añadidas las funciones de lectura y escritura, y esquema inicial del código

--- labs/lab1/modlist.c
+++ labs/lab1/modlist.c
@@ -1,28 +1,66 @@
-#include <linux/module.h>       /* Requerido por todos los módulos */
-#include <linux/kernel.h>       /* Definición de KERN_INFO */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/string.h>
+#include <linux/vmalloc.h>
+#include <linux/uaccess.h>
+
 MODULE_LICENSE("GPL");  /*  Licencia del modulo */
 
-	//Preprocesadores
+	//Preprocesadores, para escribir las funciones sin importar el órden
 int modulo_Practica1_init(void);
 void modulo_Practica1_clean(void);
+static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
+static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off);
 
 
-	/* Nodo fantasma (cabecera) de la lista enlazada */
+	//Definición de estructuras necesarias
+static const struct proc_ops proc_entry_fops = {
+	.proc_read = modlist_read,
+	.proc_write = modlist_write,    
+};
+
+		// Nodo fantasma (cabecera) de la lista enlazada
 struct list_head mylist; 
-	/* Estructura que representa los nodos de la lista */
+		//Estructura que representa los nodos de la lista
 struct list_item {
 	int data;
 	struct list_head links;
 };
 
+
+	//Variables Globales
+static struct proc_dir_entry *proc_entry;
+
+	//TODO faltan las declaraciones de memoria con kmalloc, sólo está la creación del archivo modlist en /proc  
 /* Función que se invoca cuando se carga el módulo en el kernel */
 int modulo_Practica1_init(void)
 {
-	printk(KERN_INFO "Modulo LIN cargado. Hola kernel.\n");
 
-		/* Devolver 0 para indicar una carga correcta del módulo */
+	proc_entry = proc_create("modlist", 0666, NULL, &proc_entry_fops);
+
+	if(proc_entry == NULL){
+		kfree("modlist");
+		printk("Modlist: No se pudo crear /proc/modlist entry\n");
+		return -ENOMEM;
+	}
+	else
+		printk("Módulo modlist cargado con éxito"); 
+
 	return 0;
 }
+
+	//TODO
+static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off){
+	return 0;
+}
+
+	//TODO
+static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off){
+	return 0;
+}
+
+
 
 /* Función que se invoca cuando se descarga el módulo del kernel */
 void modulo_Practica1_clean(void)
@@ -31,5 +69,5 @@ void modulo_Practica1_clean(void)
 }
 
 /* Declaración de funciones init y exit */
-module_init(modulo_Practica_init);
+module_init(modulo_Practica1_init);
 module_exit(modulo_Practica1_clean);
