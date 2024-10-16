017753a65b57a016288493be1cc549516112e3bc
alvmon02 <alvmon02@ucm.es>
2024-10-16 12:39:43 +0200

    modificado:             parteB/ledctl_invoke.c

--- labs/lab2/parteB/ledctl_invoke.c
+++ labs/lab2/parteB/ledctl_invoke.c
@@ -2,6 +2,7 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define __SYS_LED	451
 
@@ -11,17 +12,19 @@ long ledctl(unsigned int leds);
 int main(int argc, char** argv)
 {
 	unsigned int leds = 0;
-
+	
 	if(argc != 2){
 		printf("Accepts only one argument!\n");
-		return EINVAL;
+		return EXIT_FAILURE;
 	}
-	if(sscanf(argv[1], "0x%u", &leds))
-		printf("Number not inserted in correct hexadecimal format: 0x0");
-
+	
+	if((sscanf(argv[1], "0x%u", &leds)) != 1){
+		printf("Number not inserted in correct hexadecimal format: 0x(Number in Hexadecimal)\n");
+		return EXIT_FAILURE;
+	}
 	if(ledctl(leds)){
 		perror("Error en llamada sistema leds\n");
-		return 1;
+		return EXIT_FAILURE;
 	}
 
 	return 0;
@@ -30,5 +33,5 @@ int main(int argc, char** argv)
 
 
 long ledctl(unsigned int leds){
-	return (long) syscall(__HALP,leds);
+	return (long) syscall(__SYS_LED,leds);
 }
