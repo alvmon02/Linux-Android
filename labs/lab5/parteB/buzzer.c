01ce22f1fc899b58f77aece612865ff3657bfc12
alvmon02 <alvmon02@ucm.es>
2024-12-11 15:30:44 +0100

    Los trabajos diferidos y reserva de memoria ya no hacen explotar el kernel, liberación de memoria no debería de dar problemas(de momento)

--- labs/lab5/parteB/buzzer.c
+++ labs/lab5/parteB/buzzer.c
@@ -19,27 +19,17 @@
 #include <linux/timer.h>
 #include <linux/jiffies.h>
 
+#include <asm/atomic.h>
+
 MODULE_DESCRIPTION("Buzzer Kernel Module - FDI-UCM");
 MODULE_LICENSE("GPL");
-
-/* Frequency of selected notes in centihertz */
-#define C4 26163
-#define D4 29366
-#define E4 32963
-#define F4 34923
-#define G4 39200
-#define C5 52325
 
 #define PWM_DEVICE_NAME "pwmchip0"
 #define BUZZER_DEVICE_NAME "buzzer"
 
 #define MAX_LEN 256
+#define WORK_SIZE (sizeof(struct work_struct))
 
-struct pwm_device *pwm_device = NULL;
-struct pwm_state pwm_state;
-
-/* Work descriptor */
-struct work_struct my_work;
 
 /* Structure to represent a note or rest in a melodic line  */
 struct music_step
@@ -62,6 +52,15 @@ typedef enum {
 	REQUEST_NONE /* Indicador de petición ya gestionada (a establecer por tarea diferida) */
 } buzzer_request_t;
 
+//static struct workqueue_struct *my_workqueue;
+//Workqueues Descriptors
+struct workqueue_struct *my_workqueue; /* Workqueue Descriptor */
+struct work_struct *my_work_list,/* Work descriptor */
+				   *next_work;/* Work descriptor */
+
+//Buzzer Descriptions
+struct pwm_device *pwm_device = NULL;
+struct pwm_state pwm_state;
 
 static spinlock_t lock; /* Cerrojo para proteger actualización/consulta de variables buzzer_state y buzzer_request */
 static buzzer_state_t buzzer_state=BUZZER_STOPPED; /* Estado actual de la reproducción */
@@ -70,6 +69,8 @@ static buzzer_request_t buzzer_request=REQUEST_NONE;
 struct music_step *melody = NULL,
 				  *next_note = NULL;/* Puntero a la siguiente nota de la melodía actual (solo alterado por tarea diferida) */
 int bpm = 100;
+
+int workcount=0;
 
 struct timer_list buzzer_timer;
 
@@ -115,29 +116,34 @@ static int __init pwm_module_init(void)
 		return PTR_ERR(pwm_device);
 
 	err = misc_register(&buzzer_misc);
+	my_workqueue = create_workqueue("notes_queue");
 
-	if (err)
+
+
+	if(my_workqueue)
 	{
-		pr_err("Couldn't register misc device\n");
-		goto err_handle;
+		if (err) {
+			pr_err("Couldn't register misc device\n");
+			goto err_handle;
+		}
+	
+		device = buzzer_misc.this_device;
+	
+		//Asignación de memoria para melodía y siguiente nota
+		melody = vmalloc(PAGE_SIZE);
+		//next_note = kmalloc(sizeof(struct music_step),GFP_KERNEL);
+
+		//timer_setup(&buzzer_timer, timer_signal_function, 0);
+		//buzzer_time.expires = jiffies + (HZ * 1);	//Comenzar el Timer un segundo después de carga(Posiblemente innecesario)
+
+		//Activar el timer por primera vez(A lo mejor innecesario?)
+		//add_timer(&buzzer_timer);
+
+		dev_info(device, "Buzzer driver registered succesfully. To talk to\n");
+		dev_info(device, "the driver try to cat and echo to /dev/%s.\n", BUZZER_DEVICE_NAME);
+		dev_info(device, "Remove the module when done.\n");
 	}
-
-	device = buzzer_misc.this_device;
-
-	//Asignación de memoria para melodía y siguiente nota
-	melody = vmalloc(PAGE_SIZE);
-	next_note = kmalloc(sizeof(struct music_step));
-
-	timer_setup(&buzzer_timer, timer_signal_function, 0);
-	buzzer_time.expires = jiffies + (HZ * 1);	//Comenzar el Timer un segundo después de carga(Posiblemente innecesario)
-
-	//Activar el timer por primera vez(A lo mejor innecesario?)
-	add_timer(&buzzer_timer);
-
-	dev_info(device, "Buzzer driver registered succesfully. To talk to\n");
-	dev_info(device, "the driver try to cat and echo to /dev/%s.\n", BUZZER_DEVICE_NAME);
-	dev_info(device, "Remove the module when done.\n");
-
+	
 	return 0;
 err_handle:
 	return err;
@@ -172,12 +178,13 @@ static ssize_t buzzer_write(struct file *filp, const char *buff, size_t len, lof
 {
 	char c[MAX_LEN + 1],
 		*c1,
-		*c2;
+		*c2 = NULL;
 
-	int beat=0,
-		i = 0,
+	int beat = 0,
+		notes = 0,	//Contains the amount of notes the melody has
 		freq = 0,
-		len_note = 0;
+		len_note = 0,
+		i = 0;
 
 	if (copy_from_user(c, buff, len))
 		return -EFAULT;
@@ -196,33 +203,52 @@ static ssize_t buzzer_write(struct file *filp, const char *buff, size_t len, lof
 			printk(KERN_INFO "Element: %s\n",c1);
 
 			sscanf(c1,"%i:%x", &freq, &len_note);
-			melody[i].freq = freq;
-			melody[i].len = len_note;
+			melody[notes].freq = freq;
+			melody[notes].len = len_note;
 
-			printk(KERN_INFO "Nota: %i\n",melody[i].freq);
-			printk(KERN_INFO "Tiempo: %i\n",melody[i].len);
+			printk(KERN_INFO "Nota: %i\n",melody[notes].freq);
+			printk(KERN_INFO "Tiempo: %i\n",melody[notes].len);
 
 			c1 = strsep(&c2,",");
 
-			i++;
+			notes++;
 		}
+
+		my_work_list = kmalloc(WORK_SIZE * notes, GFP_KERNEL);
 	}
 	else if(sscanf(c, "beat %i", &beat) == 1)
 		bpm = beat;
 
-
 	kfree(c2);
 
-	pwm_init_state(pwm_device, &pwm_state);
+	if(my_work_list)
+	{
+		pwm_init_state(pwm_device, &pwm_state);
 
-	next_note = melody;
+		next_note = melody;
 
-	INIT_WORK(&my_work, my_wq_function);
+		i = 1;
+		next_work = my_work_list;
+		while(i < notes && next_work != NULL)
+		{
+			INIT_WORK(next_work, my_wq_function);
 
-	/* Enqueue work */
-	schedule_work(&my_work);
+			printk(KERN_INFO "Trabajo %i cargado \n",i);
+			schedule_work(next_work);
 
+			i++;
+			if(i < notes)
+			{
+				next_work = my_work_list + (i * WORK_SIZE);
+				//next_work++;
 
+			}
+		}
+
+		/* Enqueue work */
+		//schedule_work(&my_work);
+
+	}
 	return len;
 }
 
@@ -245,17 +271,26 @@ static int buzzer_release(struct inode *inode, struct file *file)
 
 static void __exit pwm_module_exit(void)
 {
+
 	/* Wait until defferred work has finished */
-	flush_work(&my_work);
+	flush_workqueue(my_workqueue);
+	destroy_workqueue(my_workqueue);
+
+	kfree(my_work_list);
 
 	/* Wait until completion of the timer function (if it's currently running) and delete timer */
-	del_timer_sync(&buzzer_timer);
+	//del_timer_sync(&buzzer_timer);
 
 	/* Release PWM device */
 	pwm_free(pwm_device);
 
+	misc_deregister(&buzzer_misc);
+
 	vfree(melody);
-	kfree(next_note);
+
+	//kfree(next_note);
+
+	printk(KERN_INFO "Module extracted Successfully\n");
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -263,7 +298,17 @@ static void __exit pwm_module_exit(void)
 /* Work's handler function */
 static void my_wq_function(struct work_struct *work)
 {
-	struct music_step *next = next_note;
+	printk(KERN_INFO "This is one work execution, number: %i\n",workcount);
+	workcount++;
+}
+static void my_wq_function2(struct work_struct *work)
+{
+	struct music_step *next;
+
+	if(next_note != NULL)
+		next = next_note;
+	else
+		return;
 
 
 	/* Obtain period from frequency */
@@ -289,14 +334,15 @@ static void my_wq_function(struct work_struct *work)
 	}
 
 	//TODO MODTIMER
-	mod_timer(buzzer_timer,jiffies + msecs_to_jiffies(calculate_delay_ms(next->len, beat)));
+	//mod_timer(buzzer_timer,jiffies + msecs_to_jiffies(calculate_delay_ms(next->len, beat)));
+	next_note++;
 	//msleep();
 }
 
 static void timer_signal_function(struct timer_list *timer)
 {
-	pwm_disable(pwm_device);
-
+	//pwm_disable(pwm_device);
+	printk(KERN_INFO "HELLO WORLD Timer Signal!!\n");
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
